fe3e6cc5597e4ca04cb948ef72ca46195efc519d
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-20 14:37:35 -0800

    a program that prints the lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main -  program that prints the lowercase alphabet in reverse
+ * main - program that prints the lowercase alphabet in reverse
  *
  * Return; 0 (Success)
  */
