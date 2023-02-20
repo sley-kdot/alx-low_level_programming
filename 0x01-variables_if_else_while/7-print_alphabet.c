410166cecdbb9f5bc64416a290e702bf15381f75
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-20 14:35:06 -0800

    a program that prints the lowercase alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  program that prints the lowercase alphabet in reverse
+ *
+ * Return; 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
