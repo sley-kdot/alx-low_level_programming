47a489da81b0a2119551f4cdcee44d4fbfccb0b5
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-20 11:44:54 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,8 +23,7 @@ int main(void)
 		printf("last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
-		printf("last digit of %d is %d and is less than 6 and not 0\n
-				", n, last_digit);
+		printf("last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	{
 	return (0);
 }
