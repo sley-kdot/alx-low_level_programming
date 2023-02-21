0037929e013f1327dc6552cbb5f51e460f6883d4
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-21 15:14:07 -0800

    added more control statement

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,14 +6,34 @@
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n < 98)
 	{
-		printf("%d", n);
-		if (n != 98)
+		while (n <= 98)
 		{
-			printf(", ");
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
 		}
-		n++;
-		printf("\n");
 	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+					printf(", ");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("98");
+	
 }
