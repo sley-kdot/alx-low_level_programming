77740f18fb0e0f6809b05c052877341d5041c3a1
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-21 13:46:07 -0800

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - functions that prints the alphabet x10 in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int count;
+	char i;
+
+	count = 0;
+	while (count <= 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		count++;
+		_putchar('\n');
+	}
+}
