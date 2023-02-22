b5a07602af20d603c8c92800650f32304e2cf0f2
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-22 02:19:24 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - function that computes the value of an integer
+ * @num: single digit input
+ * @num_abs
+ * Return: num (success)
+ */
+int _abs(int num)
+{
+	int num_abs;
+
+	num_abs = abs(num);
+	return (num_abs);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int add(int, int);
 void print_to_98(int n);
 int print_sign(int n);
+int _abs(int);
