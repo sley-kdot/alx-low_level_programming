382338969c8ed090626a022e3e3fbc6d8d007cbb
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-22 01:25:21 -0800

    fixed the if statement logic

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
-	if (i != 9)
-	{
-		putchar(',');
-		putchar(' ');
-	}
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 
 	putchar('\n');
