7a95f5c140811dae040e67078d0cdf176419dbf6
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-23 05:10:09 -0800

    fix betty style

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 /*	i = 0;*/
-/**
-*	while (i < 10)
-*	}
-*		putchar(0);
-*	}
-*/
+
+/*	while (i < 10)*/
+/*	}*/
+/*		putchar(0);*/
+/*	}*/
+
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
