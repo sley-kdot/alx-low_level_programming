ecac8a3283287c224bd29e2a8b6546a6cc0f137a
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-23 04:19:56 -0800

    testing

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+/**
+ * main - tests function that prints if integer is postive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = 0;
+	positive_or_negative(n);
+
+	return (n);
+}
