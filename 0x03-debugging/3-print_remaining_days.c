286e2b32da09232acbbb0216ea0bda1714f7480c
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-23 13:25:41 -0800

    fix error

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -5,18 +5,18 @@
  * @day: day of month
  * @year: year
  */
-
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
-
 			day++
 		}
+
 		printf("Day of the year: %d\n", day);
-		printf("Remainin days: %d\n", 366 - day);
+		printf("Remaining days: %d\n", 366 - day);
+
 	}
 	else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
 	{
@@ -24,9 +24,10 @@ void print_remaining_days(int month, int day, int year)
 		{
 			day++
 		}
-
-	printf("Day of the year: %d\n", day);
-	printf("Remaining days: %d\n", 366 - day);
+		{
+			printf("Day of the year: %\n", day);
+			printf("Remaining days: %d\n", 366 - day);
+		}
 	}
 	else
 	{
@@ -41,3 +42,20 @@ void print_remaining_days(int month, int day, int year)
 		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
