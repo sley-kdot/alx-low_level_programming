cded17a7f7d22ec7edc1a619c2f38dba958f55d5
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-23 05:07:25 -0800

    fix error

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - cause an infinity loop
+ * main - causes an infinity loop
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+/*	int i;*/
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
-/*
+/*	i = 0;*/
+/**
 *	while (i < 10)
 *	}
 *		putchar(0);
 *	}
 */
-
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
