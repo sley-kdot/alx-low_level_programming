cefa29bfa2ca412a7b37dcd1aa99fc750b1156d1
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-24 19:18:16 -0800

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a triangle
+ * @size: size of the triangle
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= (size - 1); i++)
+		{
+			for (j = 0; < (size - 1) - j; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+
+		}
+	}
+}
