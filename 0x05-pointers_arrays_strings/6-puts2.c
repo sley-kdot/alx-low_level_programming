29e43e0bad82757927a7e274729e73e9deb39fec
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-28 15:25:51 -0800

    print other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: character input
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\n')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
