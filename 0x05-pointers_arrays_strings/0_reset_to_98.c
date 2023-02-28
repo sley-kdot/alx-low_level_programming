18e923e69a357526119e4b77a2e2237a788215cc
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-28 13:10:03 -0800

    rest to 98

---
+++ 0x05-pointers_arrays_strings/0_reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - fnction that takes a pointer to an in as parameter and updates
+ *  the value it points to to 98
+ *  @n: single digit input
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
