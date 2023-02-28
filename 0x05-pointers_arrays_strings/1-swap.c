d102bb4fb844d3e571754f0098a124d78c8b8c40
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-02-28 13:19:51 -0800

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: first digit input
+ * @b: second digit input
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	n = *b;
+}
