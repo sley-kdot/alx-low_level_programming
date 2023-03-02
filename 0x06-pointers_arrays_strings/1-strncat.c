16515ada270226f9be9caa5514c2fa502b5e536f
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-02 13:24:23 -0800

    Concatenates to strings 2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - functions that concatenates two strings
+ * @dest: first character input
+ * @src: second character input
+ * @n: third input digit
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *nm = dest;
+
+	while (*nm != '\0')
+	{
+		nm++;
+	}
+
+	while (*src != '\0' && n -- > 0)
+	{
+		*nm = *src++;
+	}
+	*nm = '\0';
+
+	return (dest);
+}
