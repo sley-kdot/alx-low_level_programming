6767dd98c03a9b867079f23f34bd8f076e38a7fb
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-03 17:04:31 -0800

    made simple changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 /**
  * *_strncpy - functions that copies a string
  * @dest: first character input
@@ -10,13 +10,5 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		if (i < n)
-			dest[i] = src[i];
-		while (i < n)
-			dest[i++] = '\0';
-
-	return (dest);
+	return (strncpy(dest, src, n));
 }
