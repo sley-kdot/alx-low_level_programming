e4fb597484133bc313a997a109ed93eaa2aab989
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-07 11:33:22 -0800

    Task 5 - reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverses a string
+ * @s: single input character
+ */
+
+void rev_string(char *s)
+{
+	int count, i;
+
+	count = strlen(s);
+
+	for (i = (count - 1); i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+}
