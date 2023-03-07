b5361b72dac10822eb14975140fb45340a013969
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-07 12:05:38 -0800

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - function thay prints n elements of an array of integer
+ * @a: first input char
+ * @n: second inpu digit
+ */
+
+void print_array(char *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	_putchar('\n');
+}
