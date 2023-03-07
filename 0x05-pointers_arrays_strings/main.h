431f67cabf34fe2e7258cc30f158f6c0d9384604
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-07 11:42:44 -0800

    added prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,6 +8,9 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 void puts2(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
 
 #endif /* MAIN_H */
