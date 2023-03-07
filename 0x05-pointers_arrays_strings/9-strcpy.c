fab5f12f8ac83eee3cf3905a907a170c3086f267
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-07 11:47:24 -0800

    Task 9 - copies string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcpy - copies the string pointed by the src
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
