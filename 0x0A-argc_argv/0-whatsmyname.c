4337d9e78c7a079a0cd1f738857235cc6e924571
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-12 11:22:42 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description: prints its name, followed by newline.
+* @argc: argument counter
+* @argv: argument vector
+* Return: Always 0 (success)
+*/
+
+int main(unsigned int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
