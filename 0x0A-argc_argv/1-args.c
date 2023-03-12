28b08ec4fb0d753632aabc5eea0e20cb048a3430
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-12 11:43:51 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,12 +5,12 @@
 * Description: prints the number of arguments passed into it.
 * @argc: argument counter
 * @argv: argument vector
-
+*
 * Return: Always 0 (success)
 */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
