a09fe78447e09f4d62399c7db001f07d40c12f75
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-19 08:41:33 -0700

    fix output

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int *) * width);
+		arr[i] = malloc(sizeof(int **) * width);
 		if (arr == NULL)
 		{
 			while (i-- > 0)
