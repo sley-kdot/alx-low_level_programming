8f08c2c9411bb4ed39cdfbecb302b323ed37d31e
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-19 16:27:05 -0700

    fix betty style

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,12 +18,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 = "";	
+		s2 = "";
 
 	while (s1[len1])
 		len1++;
 
- 	while (s1[len2])
+	while (s2[len2])
 		len2++;
 
 	if (n >= len2)
