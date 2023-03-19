db51e86b5afa79d4a0914969d0081d6b238db4a3
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-19 06:23:58 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+
+
+/**
+* *str_concat - function that concatenates two strings.
+* @s1: first input
+* @s2: second input
+* Return: a pointer
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, k, l;
+	char *strout;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (i + j));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (k = 0; s1[k] != '\0'; k++)
+	{
+		strout[k] = s1[k];
+	}
+	for (l = 0; s2[l] != '\0'; l++)
+	{
+		strout[k + l] = s2[l];
+	}
+	return (strout);
+}
