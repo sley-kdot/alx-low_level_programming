179b4062c2bd7be03866f864c1dbfc3637fd9efa
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-20 15:26:06 -0700

    Task 0 - Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+* struct dog - struct that store info of a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
