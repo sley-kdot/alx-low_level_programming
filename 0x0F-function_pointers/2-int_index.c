da7766724f047de10822e0def9ae0ec9997fa953
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-23 04:28:39 -0700

    fixed and locally tested

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,22 +9,19 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if (array && cmp)
+	if (array == NULL)
+		return (-1);
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-		{
-			return (-1);
-		}
-		while (i < size)
-		{
-			if (cmp(array[i] != 0))
-			{
-				return (i);
-			}
-		}
-
+		if (cmp(array[i] != 0))
+			return (i);
 	}
 	return (-1);
 }
