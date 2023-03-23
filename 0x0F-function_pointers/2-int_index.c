ee0c6a6f843945ac81b503c66bfddbbd94de2676
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-22 17:01:40 -0700

    fix betty

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
 * int_index - searches for an integer
-* array: name of array
+* @array: name of array
 * @size: size of array
 * @cmp: function pointer
 *
@@ -19,7 +19,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 		}
 		while (i < size)
 		{
-			if (cmp(array[i] !=0))
+			if (cmp(array[i] != 0))
 			{
 				return (i);
 			}
