741d12529a6b50b6b21281f932b3a9094b9b0295
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-03-25 16:52:02 -0700

    Made all files executable

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* puts_half - prints half of a string, followed by a new line.
+* @str: input string
+*
+* Return: void
+*/
+void puts_half(char *str)
+{
+	int i, n, len = 0;
+
+	while (str[i])
+		len++;
+
+	n = (len - 1) / 2 + 1;
+
+	for (i = n; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
