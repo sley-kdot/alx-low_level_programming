8d590d32a85bca552f5aea932a3d862c4bdde34b
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-04-09 20:23:09 -0700

    locally tested initial commit

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* get_endianness - checks the endianness
+*
+* Return: 0 if big endian, 1 if little endian
+*/
+int get_endianness(void)
+{
+	char *c;
+	int num = 1;
+
+	c = (char *)(&num);
+
+	if (*c == 1)
+		return (0);
+	else
+		return (1);
+}
