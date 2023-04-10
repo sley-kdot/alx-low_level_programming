2b78eef36b8882492b0cf6e1eaf7f55e017e1dba
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-04-09 20:36:17 -0700

    fixed: typo in header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,6 +9,6 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-nt get_endianness(void);
+int get_endianness(void);
 
 #endif
