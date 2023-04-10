415b97159d5f5a4ad1f38d9871d8d311b6f13cdb
Nnamdi Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-04-09 19:23:33 -0700

    locally tested initial commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_binary - prints the binary representationof a number
+* @n: number to be represented as binary
+*
+* Return: void
+*/
+void print_binary(unsigned long int n)
+{
+	unsigned long int temp;
+
+	if (n > 1)
+	{
+		temp = (n >> 1);
+		print_bin(temp);
+	}
+	_putchar((n & 1) + '0');
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,5 +3,5 @@
 
 #include <stdio.h>
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 #endif
