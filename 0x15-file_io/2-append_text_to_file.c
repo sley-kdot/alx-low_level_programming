b350f58c468c5564f473fe2142cb2cc3d1e7fa5b
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2023-07-15 08:20:22 -0700

    fix code

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,8 +10,11 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, len = 0; 
+	int fd, len = 0;
 	ssize_t w_file;
+
+	if (!filename)
+		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
