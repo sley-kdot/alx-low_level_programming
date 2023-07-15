40a154fa5d8ab14d2a0cabe9898638046811592f
Nnamdi Okonmah <nnamdiokonma@gmail.com>
2023-07-15 08:15:41 -0700

    rm wrong identifier

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		len++;
 
 	if (text_content != NULL)
-	{i
+	{
 		w_file = write(fd, text_content, len);
 		if (w_file == -1)
 			return (-1);
