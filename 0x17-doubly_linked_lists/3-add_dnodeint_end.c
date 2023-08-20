6252cc14e07738eacb17b3e224dd1ac2cc0834c5
Kingsley Okonmah <nnamdiokonma@gmail.com>
2023-08-20 14:54:36 -0700

    fixed compilation error

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -31,7 +31,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		while (h->next != NULL)
 			h = h->next;
 		h->next = new;
-		new->prev = *h;
+		new->prev = h;
 	}
 	return (new);
 }
